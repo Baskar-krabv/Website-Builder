905a877ddd16f3b423f2c56876d7ea9bd813a73d
Sri-Akash <sriakash152@gmail.com>
2024-09-12 19:22:11 +0530

    Updated

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -3,57 +3,73 @@ import $ from 'jquery';
 import 'jquery-ui/ui/widgets/sortable';
 import 'jquery-ui/ui/widgets/draggable';
 import 'jquery-ui/ui/widgets/resizable';
-import 'jquery-ui/themes/base/all.css';  
+import 'jquery-ui/themes/base/all.css';
 export const FrontPage = () => {
   const [color, Setcolor] = useState('')
-  const changeColor = (bgcolor) => {
-    Setcolor(bgcolor)
+  const changeColor = () => {
+    Setcolor(document.getElementById('col').value)
   }
+  
   const text = () => {
+    const col=document.createElement('input')
+    col.type="color"
+    col.setAttribute('class','color')
     const div = document.createElement('div')
-    const del=document.createElement('i')
-    del.setAttribute('class','fa-solid fa-trash')
+    const del = document.createElement('i')
+    const aplay=document.createElement('i')
+    aplay.setAttribute('class',"fa-solid fa-check")
+    del.setAttribute('class', 'fa-solid fa-trash')
     div.setAttribute("class", "textDiv")
-    const newTextEle=document.createElement("p")
+    const newTextEle = document.createElement("p")
     const inpt = document.createElement('input')
     inpt.setAttribute("class", "inpt")
     newTextEle.setAttribute("class", "para")
     document.querySelector(".editor").appendChild(inpt)
-    inpt.addEventListener('mouseleave',()=>{
-      newTextEle.textContent=inpt.value
-      inpt.style.display="none"
+    inpt.addEventListener('mouseleave', () => {
+      newTextEle.textContent = inpt.value
+      inpt.style.display = "none"
+      div.appendChild(aplay)
+      div.appendChild(col)
       div.appendChild(del)
       div.appendChild(newTextEle)
       document.querySelector(".editor").appendChild(div)
-      $('.textDiv').draggable({ containment: '#Editor1',scroll: false });
-      $( ".para" ).resizable({
-        containment: ".editor"
-      }) 
+      $('.textDiv').draggable({ containment: '#Editor1', scroll: false });
+      $(".para").resizable({
+        containment: "#Editor1"
+      })
     })
-    newTextEle.addEventListener('click',()=>{
-      newTextEle.style.display="none"
-      div.style.display="none"
-      inpt.style.display="block"
-      inpt.addEventListener('mouseleave',()=>{
-        newTextEle.textContent=inpt.value
-        inpt.style.display="none"
-        newTextEle.style.display="block"
-        div.style.display="block"
+    newTextEle.addEventListener('click', () => {
+      newTextEle.style.display = "none"
+      div.style.display = "none"
+      inpt.style.display = "block"
+      inpt.addEventListener('mouseleave', () => {
+        newTextEle.textContent = inpt.value
+        inpt.style.display = "none"
+        newTextEle.style.display = "block"
+        div.style.display = "block"
         div.appendChild(newTextEle)
         document.querySelector(".editor").appendChild(div)
-        $('.textDiv').draggable({ containment: '#Editor1',scroll: false });
-        $( ".para" ).resizable({
-          containment: ".editor"
+        $('.textDiv').draggable({ containment: '#Editor1', scroll: false });
+        $(".para").resizable({
+          containment: ".#Editor"
         })
       })
     })
+    document.querySelector('.fa-check').addEventListener('click',()=>{
+      document.querySelector('.para').color=document.querySelector('.color').value
+  })
+    del.addEventListener('click', () => {
+      document.querySelector(".editor").removeChild(div)
+    })
+    
   }
+  
   const btn = () => {
-    const div=document.createElement('div')
+    const div = document.createElement('div')
     div.setAttribute("class", "textDiv")
-    const del=document.createElement('i')
-    del.setAttribute('class','fa-solid fa-trash')
-    const newTextEle=document.createElement("div")
+    const del = document.createElement('i')
+    del.setAttribute('class', 'fa-solid fa-trash')
+    const newTextEle = document.createElement("div")
     const inpt = document.createElement('button')
     newTextEle.setAttribute('id', 'btn_add')
     inpt.textContent = "Click here"
@@ -61,18 +77,21 @@ export const FrontPage = () => {
     newTextEle.appendChild(inpt)
     div.appendChild(newTextEle)
     document.querySelector(".editor").appendChild(div)
-    $('.textDiv').draggable({ containment: '#Editor1',scroll: false });
+    $('.textDiv').draggable({ containment: '#Editor1', scroll: false });
+    del.addEventListener('click', () => {
+      document.querySelector(".editor").removeChild(div)
+    })
   }
 
   const img = () => {
-    const div=document.createElement('div')
-    div.setAttribute("class", "textDiv")
-    const del=document.createElement('i')
-    del.setAttribute('class','fa-solid fa-trash')
+    const div = document.createElement('div')
+    div.setAttribute("class", "textdiv")
+    const del = document.createElement('i')
+    del.setAttribute('class', 'fa-solid fa-trash')
     const fileRd = document.createElement('input')
     fileRd.type = "file"
     const imgEle = document.createElement('img')
-    imgEle.setAttribute("class","image")
+    imgEle.setAttribute("class", "image")
     document.querySelector(".editor").appendChild(fileRd)
     fileRd.addEventListener('change', () => {
       const imgSrc = fileRd.files[0]
@@ -86,68 +105,89 @@ export const FrontPage = () => {
         div.appendChild(del)
         div.appendChild(imgEle)
         document.querySelector(".editor").appendChild(div)
-        $('.textDiv').draggable({ containment: '#Editor1',scroll: false });
-        $( ".textDiv" ).resizable({
+        $('.textdiv').draggable({ containment: '#Editor1', scroll: false });
+        $(".textdiv").resizable({
           containment: ".editor"
         })
       }
     })
+    del.addEventListener('click', () => {
+      document.querySelector(".editor").removeChild(div)
+    })
   }
-    const vid_add = () => {
-      const fileRd = document.createElement('input')
-      fileRd.type = "file"
-      const vidEle = document.createElement("video")
-      vidEle.setAttribute('id',"vid")
-      document.querySelector(".editor").appendChild(fileRd)
-      fileRd.addEventListener('change', () => {
-        const vidSrc = fileRd.files[0]
-        if (vidSrc) {
-          const reader = new FileReader()
-          reader.onload = function (e) {
-            vidEle.src = e.target.result
-          }
-          reader.readAsDataURL(vidSrc)
-          fileRd.style.display = "none"
-          document.querySelector(".editor").appendChild(vidEle)
-          $('#vid').draggable({ containment: '#Editor1',scroll: false });
-
+  const vid_add = () => {
+    const div = document.createElement('div')
+    div.setAttribute("class", "textdiv")
+    const del = document.createElement('i')
+    del.setAttribute('class', 'fa-solid fa-trash')
+    const fileRd = document.createElement('input')
+    fileRd.type = "file"
+    const vidEle = document.createElement("video")
+    vidEle.setAttribute('id', "vid")
+    document.querySelector(".editor").appendChild(fileRd)
+    fileRd.addEventListener('change', () => {
+      const vidSrc = fileRd.files[0]
+      if (vidSrc) {
+        const reader = new FileReader()
+        reader.onload = function (e) {
+          vidEle.src = e.target.result
         }
-      })
-    }
+        reader.readAsDataURL(vidSrc)
+        fileRd.style.display = "none"
+        div.appendChild(del)
+        div.appendChild(vidEle)
+        document.querySelector(".editor").appendChild(div)
+        $('#vid').draggable({ containment: '#Editor1', scroll: false });
+        del.addEventListener('click', () => {
+          document.querySelector(".editor").removeChild(div)
+        })
+      }
+    })
+  }
 
 
-    return (
-      <div className="main">
-        <header>
-          <div className="name">
-            <i className="fa-solid fa-feather"></i>
-            <h1>WebSite Builder</h1>
+  return (
+    <div className="main">
+      <header>
+        <div className="name">
+          <i className="fa-solid fa-feather"></i>
+          <h1>WebSite Builder</h1>
+        </div>
+      </header>
+      <div className="builder">
+        <div className="build__area">
+          <div className="editor" id="Editor1" style={{ background: color }}>
+
           </div>
-        </header>
-        <div className="builder">
-          <div className="build__area">
-            <div className="editor" id="Editor1" style={{ background: color }}>
-              
-            </div>
-            {/* {$('#Editor1').sortable()} */}
-          </div>
-          <div className="design__area">
-            <h3>Tools</h3>
-            <div className="formatting__tools">
+          {/* {$('#Editor1').sortable()} */}
+        </div>
+        <div className="design__area">
+          <h3>Tools</h3>
+          <div className="formatting__tools">
+            <div className="tool">
+              <h4>Text</h4>
               <i onClick={text} className="fa-solid fa-font"></i>
+            </div>
+            <div className="tool">
+              <h4>Image</h4>
               <i onClick={img} className="fa-regular fa-image"></i>
+            </div>
+            <div className="tool">
+              <h4>Button</h4>
               <i onClick={btn} className="fa-solid fa-toggle-on"></i>
+            </div>
+            <div className="tool">
+              <h4>Video</h4>
               <i onClick={vid_add} className="fa-solid fa-file-video"></i>
             </div>
-            <h3>Colors</h3>
-            <div className="templates">
-              <button onClick={() => changeColor('linear-gradient(90deg,#2629A8,#FA0E94)')}><img src="2328019.jpg" alt="" className="img1" /></button>
-              <button onClick={() => changeColor('linear-gradient(90deg,#E26B89,#A660A6)')}><img src="5419262.jpg" alt="" className="img2" /></button>
-              <button onClick={() => changeColor('linear-gradient(90deg,#151821,#151821)')}><img src="5943732.jpg" alt="" className="img3" /></button>
-              <button onClick={() => changeColor('linear-gradient(90deg,#FB567F,#FB567F)')}><img src="6701820.jpg" alt="" className="img4" /></button>
-            </div>
+          </div>
+          <h3>Colors</h3>
+          <div className="templates">
+            <input type="color" id="col" />
+            <button onClick={changeColor}>SetBackgroundColor</button>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
